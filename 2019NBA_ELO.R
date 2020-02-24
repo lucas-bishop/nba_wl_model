7fbc5889d80f54911110f7c4394507a2b4659d15
Lucas Bishop <bishoplucas95@gmail.com>
2020-02-24 17:39:45 -0500

    updated code

--- 2019NBA_ELO.R
+++ 2019NBA_ELO.R
@@ -17,5 +17,13 @@ all_game_data <- read_csv(file="nba_elo.csv",
                                      score1=col_integer(),
                                      score2=col_integer())
                       ) %>% filter(date < current_date)
+# make df for this only this season
+current_season <- all_game_data %>% filter(season == "2020")
+
+# add new columns that show the favorite and if they won based on ELO model
+favorite_win_prob <- all_game_data %>%
+  mutate(fav_538_won=ifelse(elo_prob1>elo_prob2, score1 > score2, score2 > score1),
+         fav_538_prob=ifelse(elo_prob1>elo_prob2, elo_prob1, elo_prob2)) %>%
+  select(season, date, team1, team2, fav_538_won, fav_538_prob)
 
 
