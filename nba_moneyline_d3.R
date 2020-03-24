72d614918657ad7e8ca0f602180ce4ba7c5c1938
Lucas Bishop <bishoplucas95@gmail.com>
2020-03-24 14:44:50 -0400

    new script for d3 moneyline analysis

---
+++ nba_moneyline_d3.R
@@ -0,0 +1,16 @@
+## Day 3 of NBA win prob modeling, this script will focus on how people have betted on games
+# To begin we need to find the data that includes betting favorites and history of this data
+
+# To do this we will need to scrape some data from online oddsbooks
+# We need date, name of the teams, the scores, and the moneylines for each game
+
+sportsbook_html <- read_html("https://www.sportsbookreview.com/betting-odds/nba-basketball/money-line/?date=20100306")
+
+# picking nodes with only the information you need is tricky- depending on the webpage 
+# some of the div's will have more logical names than the rnadom letters and numbers here
+
+team_names <- sportsbook_html %>% html_nodes("div._3A-gC._2DWLf") %>% .[[1]] %>% 
+  html_nodes("span._3O1Gx") %>% html_text()
+
+scores <- sportsbook_html %>% html_nodes("div._3A-gC._2DWLf") %>% .[[1]] %>% 
+  html_nodes("div._1Y3rN._2trL6") %>% html_text()
