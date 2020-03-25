f1c9ca6f447536ad24e0472b43ee7da2e7e649e4
Lucas Bishop <bishoplucas95@gmail.com>
2020-03-25 16:53:06 -0400

    commented out new html portion of script so I don't need to write two different functions for same hting

--- nba_moneyline_d3.R
+++ nba_moneyline_d3.R
@@ -4,27 +4,34 @@
 # To do this we will need to scrape some data from online oddsbooks
 # We need date, name of the teams, the scores, and the moneylines for each game
 
-sportsbook_html <- read_html("https://www.sportsbookreview.com/betting-odds/nba-basketball/money-line/?date=20100306")
-
-# picking nodes with only the information you need is tricky- depending on the webpage 
-# some of the div's will have more logical names than the rnadom letters and numbers here
-
-team_names <- sportsbook_html %>% html_nodes("div._3A-gC._2DWLf") %>% .[[1]] %>% 
-  html_nodes("span._3O1Gx") %>% html_text()
-
-scores <- sportsbook_html %>% html_nodes("div._3A-gC._2DWLf") %>% .[[1]] %>% 
-  html_nodes("div._1Y3rN._2trL6 div:nth-child(n)") %>%
-  html_text()
-
+#  sportsbook_html <- read_html("https://www.sportsbookreview.com/betting-odds/nba-basketball/money-line/?date=20100306")
 ### I realized the problems with the strange html code is from not being in classic view.
 ### I amgoing to create a new variable for the html of the classic view url to do rest of the analysis and then Ican merge
 
-old_html <- read_html("https://classic.sportsbookreview.com/betting-odds/nba-basketball/?date=20100306")
+library(tidyverse)
+library(lubridate)
+library(rvest)
 
-date <- sportsbook_html %>% html_nodes("div._2STB6") %>% html_text()
+classic_html <- read_html("https://classic.sportsbookreview.com/betting-odds/nba-basketball/money-line/?date=20100306")
 
-moneyline <- old_html %>% html_nodes("div.holder-complete") %>% .[[1]] %>%
-  html_nodes("div.eventLine-book") %>% 
-  html_text()
+parse_table_row <- function(row){
+  team_names <- row %>% 
+    html_nodes("span.team-name") %>% html_text()
+  
+  scores <- row %>% 
+    html_nodes("span.current-score") %>%
+    html_text()
+  
+  moneylines <- row %>%
+    html_node("div.eventLine-book") %>% html_nodes("div.eventLine-book-value") %>% 
+    html_text()
+  
+  c(team_names, scores, moneylines)
+}
+
+
+classic_html %>% html_nodes("div.holder-complete") %>% map(., parse_table_row)
+  
+  
 
 
