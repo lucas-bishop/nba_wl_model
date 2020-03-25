c4be68e58a7ba5637fc2ad2579edc485081c3706
Lucas Bishop <bishoplucas95@gmail.com>
2020-03-25 14:29:37 -0400

    made switch from new html page view to old to make scraping easier. wish I knew to use classic view before starting. sigh

--- nba_moneyline_d3.R
+++ nba_moneyline_d3.R
@@ -16,6 +16,15 @@ scores <- sportsbook_html %>% html_nodes("div._3A-gC._2DWLf") %>% .[[1]] %>%
   html_nodes("div._1Y3rN._2trL6 div:nth-child(n)") %>%
   html_text()
 
-date <- sportsbook_html
+### I realized the problems with the strange html code is from not being in classic view.
+### I amgoing to create a new variable for the html of the classic view url to do rest of the analysis and then Ican merge
 
-moneylines <- sportsbook_html
+old_html <- read_html("https://classic.sportsbookreview.com/betting-odds/nba-basketball/?date=20100306")
+
+date <- sportsbook_html %>% html_nodes("div._2STB6") %>% html_text()
+
+moneyline <- old_html %>% html_nodes("div.holder-complete") %>% .[[1]] %>%
+  html_nodes("div.eventLine-book") %>% 
+  html_text()
+
+
